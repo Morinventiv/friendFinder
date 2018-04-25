ad8bfb806036271ab8477470ed1272b83ff2d02b
Morinventiv <brandonmorin@gmail.com>
2018-04-25 16:33:46 -0600

    added initial files

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendArray = [
+	{
+		'name': 'Will',
+		'photo': 'https://qph.fs.quoracdn.net/main-qimg-5c90ba638048439a00a3f17f46cdb70e-c',
+		'scores': [
+			5,
+			4,
+			1,
+			2,
+			4,
+			5,
+			3,
+			5,
+			4,
+			3
+		]
+	},
+	{
+		'name': 'Vinny',
+		'photo': 'https://static.makeuseof.com/wp-content/uploads/2014/01/creepy-guy.jpg',
+		'scores': [
+			3,
+			1,
+			2,
+			4,
+			2,
+			3,
+			1,
+			3,
+			3,
+			5
+		]
+	},
+	{
+		'name': 'Logaine',
+		'photo': 'https://i.pinimg.com/736x/d4/7c/50/d47c50c51f1483d07783502ee266a2a0.jpg',
+		'scores': [
+			4,
+			3,
+			5,
+			1,
+			2,
+			5,
+			2,
+			1,
+			4,
+			5
+		]
+	},
+	{
+		'name': 'Marty',
+		'photo': 'https://www.oddee.com/_media/imgs/articles/a99_feldman.jpg',
+		'scores': [
+			5,
+			3,
+			4,
+			3,
+			4,
+			1,
+			5,
+			3,
+			5,
+			3
+		]
+	},
+	{
+		'name': 'Johnnie',
+		'photo': 'https://mk0artfidblogopf5abx.kinstacdn.com/wp-content/uploads/2014/12/funny-beard-styles-incredibeard-5.jpg',
+		'scores': [
+			4,
+			2,
+			2,
+			5,
+			3,
+			1,
+			4,
+			2,
+			2,
+			5
+		]
+	}
+];
+
+module.exports = friendArray;
